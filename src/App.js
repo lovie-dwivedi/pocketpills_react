ee24ea2e514f20edcf7c11e416af85f0f5575858
arvindpmfby <arvind.pmfby@gmail.com>
2021-02-02 19:34:11 +0530

    pocketpills in react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HowItWorks from './HowItWorks';
+import HomePage from './HomePage';
+import OurServices from './OurServices';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  sessionStorage.setItem("lang", "english")
+
+  return (
+    <>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path = '/' component = { HomePage  } />
+        <Route exact path = '/HowItWorks' component = { HowItWorks  } />
+        <Route exact path = '/OurServices' component = { OurServices  } />
+      </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
